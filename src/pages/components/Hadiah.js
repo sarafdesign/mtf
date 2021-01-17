9ce636b1c045441f9e5194061b7b91c70cacedba
Anas Alqoyyum <doublezero13@gmail.com>
2021-01-17 22:56:17 +0700

    Hadiah Rev

--- src/pages/components/Hadiah.js
+++ src/pages/components/Hadiah.js
@@ -2,18 +2,18 @@ import React from "react";
 import "../assets/css/Hadiah.css";
 import Trophy from "../assets/img/trophy.svg";
 import Hand from "../assets/img/Saly-24.svg";
-import Bulet from "../assets/img/Side elements2.svg"
+import Bulet from "../assets/img/Side elements2.svg";
 
 const Hadiah = () => {
   return (
     <>
       <div className="background">
         <img src={Trophy} alt="" className="trophy" />
-        <img src={Bulet} alt="" className="bulet"/>
+        <img src={Bulet} alt="" className="bulet" />
         <section className="container hadiah">
           <div className="top">
             <h1>Hadiah Peserta</h1>
-            <br/>
+            <br />
             <h3>
               Selain mendapatkan pengalaman yang berharga dan mengembangkan
               kompetensi kamu, kamu juga berkesempatan memenangkan hadiah
@@ -24,19 +24,28 @@ const Hadiah = () => {
           <img src={Hand} alt="" className="hand" />
           <div className="row justify-content-center">
             <div className="kotak col-4 col-md-4 col-lg-4">
-              <h2>Juara 1</h2>
-              <p>Rp. 50.000.000</p>
-              <p className="teksduit">(Lima Puluh Juta Rupiah)</p>
+              <h2>
+                Juara 1 <br />
+                <br />
+                <p>Rp. 50.000.000</p>{" "}
+                <p className="teksduit">(Lima Puluh Juta Rupiah)</p>
+              </h2>
             </div>
             <div className="kotak col-4 col-md-4 col-lg-4">
-              <h2>Juara 2</h2>
-              <p>Rp. 20.000.000</p>
-              <p className="teksduit">(Dua Puluh Juta Rupiah)</p>
+              <h2>
+                Juara 2 <br />
+                <br />
+                <p>Rp. 20.000.000</p>{" "}
+                <p className="teksduit">(Dua Puluh Juta Rupiah)</p>
+              </h2>
             </div>
             <div className="kotak col-4 col-md-4 col-lg-4">
-              <h2>Juara 3</h2>
-              <p>Rp. 10.000.000</p>
-              <p className="teksduit">(Sepuluh Juta Rupiah)</p>
+              <h2>
+                Juara 3 <br />
+                <br />
+                <p>Rp. 10.000.000</p>{" "}
+                <p className="teksduit">(Sepuluh Juta Rupiah)</p>
+              </h2>
             </div>
           </div>
         </div>
